5f15e41bbbaefa56e5c47ea7b0c371c6b84dda41
firdausfikriii <firdaus24ti@mahasiswa.pcr.ac.id>
2026-04-13 20:22:08 +0700

    Pertemuan 4 Lanjutan

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById("root"))
     .render(
         <div >
         {/* <TailwindCSS/> */}
-        <UserForm/>
+        {/* <UserForm/> */}
         <HitungGajiForm/>
         </div>
     )
